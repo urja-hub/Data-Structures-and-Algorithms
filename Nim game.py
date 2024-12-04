c783b2abd7aad989be0a71114fc8db273d75fab8
urja-hub <urja0506@gmail.com>
2024-12-04 13:09:00 -0500

    Uploading Nim Game.py Day 4

---
+++ Nim game.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return n % 4 != 0
+
+expected, n = False, 4
+actual = Solution().canWinNim(n)
+assert expected == actual, "Mistake in test case 1"
+
+expected, n = True, 2
+actual = Solution().canWinNim(n)
+assert expected == actual, "Mistake in test case 2"
+
+expected, n = True, 1
+actual = Solution().canWinNim(n)
+assert expected == actual, "Mistake in test case 3"
+
+print('All tests pass!')
