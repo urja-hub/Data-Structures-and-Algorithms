751db6ffa2d36896947002327a18c6f87ffe7e5e
urja-hub <urja0506@gmail.com>
2025-01-21 15:40:50 -0500

    Delete function heap.py :  Day 15

--- Heap.py
+++ Heap.py
@@ -1,5 +1,6 @@
 # Create a function for heap 
-
+# This is almost good for now, but check how can you make it to original index and check for alternative solutions as well.
+# Remember I'm doing the heapify up(top - down approach) procedure, code for heapify down, or bottom up approach
 def create_max_heap(arr):
     arr.insert(0,0)
     for i in range(1, len(arr)):
@@ -11,6 +12,41 @@ def create_max_heap(arr):
 
     arr.pop(0)
     return arr
+
+# Create a function to delete the elements
+
+def delete_element(arr):
+
+    arr.insert(0,0)
+    arr[1] = arr[-1]
+    arr.pop()
+    current = 1
+    while 2 * current < len(arr):
+        if 2 * current + 1 < len(arr):
+            if arr[2 * current] > arr[2 * current +1]:
+                if arr[2 * current] > arr[current]:
+                    arr[2*current], arr[current] = arr[current], arr[2 * current]
+                    current = 2 * current
+                else:
+                    break
+            else:
+                if arr[2*current] < arr[2*current + 1]:
+                    arr[2*current + 1], arr[current] = arr[current], arr[2 * current + 1]
+                    current = 2 * current + 1
+                else: 
+                    break
+        
+        else:
+            if arr[2 * current] > arr[current]:
+                arr[2 * current], arr[current] = arr[current],arr[2 * current]
+            
+            break
+
+        
+
+    arr.pop(0)
+    return arr
+    
 
 ''' while j < len(arr):
         if arr[i] > arr[i//2]:
@@ -29,3 +65,4 @@ def create_max_heap(arr):
 '''
 arr = [1,2,3,4,5,93,23,0,43]
 print(create_max_heap(arr))
+print(delete_element(arr))
